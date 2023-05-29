1db38ae50fbcf247c81f82f7fcc8abaed7a4602b
DomenicoLucarelli <domyluca@gmail.com>
2023-05-29 17:06:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+    data() {
+        return {};
+    },
+    
+    components: { AppMain }
+}
+</script>
+
+<template>
+
+  <AppMain></AppMain>
+
+</template>
+
+<style >
+
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+
+    name: 'AppMain',
+
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        ciao
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
