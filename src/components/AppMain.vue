7b1d74d17d7bd62c2f2e5ec39e68ccb20d8340b4
DomenicoLucarelli <domyluca@gmail.com>
2023-05-29 17:18:38 +0200

    feat: chiamata axios avvenuta correttamente,  popolamento dell'array works

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,30 @@
 <script>
+import axios from 'axios';
 export default {
 
     name: 'AppMain',
 
     data() {
         return {
+           
+            works: [],
             
         }
+    },
+
+    created() {
+
+        axios.get('http://127.0.0.1:8000/api/works').then((res)=>{
+            this.works = res.data.results
+        });
+            
     },
 }
 </script>
 
 <template>
     <div class="container">
-        ciao
+        
     </div>
 </template>
 
